f5ce75bb998525a5b48a1e66d90c91805a54f810
Chetan Bhat <chetanbhat999@gmail.com>
2021-03-05 17:54:31 +0000

    test with python file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const spawn = require('child_process').spawn;
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,14 @@ app.get('/', function( req, res){
     res.send('hello from server');
 }
 );
+
+app.get('/insert', function( req, res){
+
+    const process = spawn('python', ['./pythonFiles/test.py']);
+    process.stdout.on('data', (data)=> {
+        res.send('pthon file data is ' + data.toString());
+    })
+});
 
 app.listen(PORT, function(){
     console.log("server is running on localhost");
